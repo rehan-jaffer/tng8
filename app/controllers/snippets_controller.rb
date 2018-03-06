22a5f4e0312f30b94794867c0a5b173e3d2906b6
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-06 17:25:21 +0000

    added initial RESTful routes

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,2 +1,26 @@
 class SnippetsController < ApplicationController
+
+  def new
+    @snippet = Snippet.new
+  end
+
+  def create
+    @snippet = Snippet.new(snippet_params)
+    if @snippet.save
+      redirect_to snippet_path(@snippet)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @snippets = Snippet.all
+  end
+
+  private
+
+    def snippet_params
+      params.require(:snippet).permit(:content)
+    end
+
 end
