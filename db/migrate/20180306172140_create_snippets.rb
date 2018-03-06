009be0ff1cea488f5b385164155fb1e962f1a2c9
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-06 17:26:50 +0000

    added migration for snippets table

--- db/migrate/20180306172140_create_snippets.rb
+++ db/migrate/20180306172140_create_snippets.rb
@@ -1,7 +1,9 @@
 class CreateSnippets < ActiveRecord::Migration[5.1]
   def change
     create_table :snippets do |t|
-
+      t.string :content, null: false
+      t.integer :score, default: 0
+      t.integer :ratio, default: 1
       t.timestamps
     end
   end
